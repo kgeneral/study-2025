e2849b62d1e489c379b7b396829888c2dc30fd16
kgeneral <bruce.daeyeong.kim@gmail.com>
2025-02-24 19:39:43 +0900

    heap mem test api

--- architecture/src/main/kotlin/com/bruce/study/architecture/ArchitectureApplication.kt
+++ architecture/src/main/kotlin/com/bruce/study/architecture/ArchitectureApplication.kt
@@ -5,12 +5,21 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-data class Response(val message: String)
+data class Response(val nanotime:Long, val message: String)
 
 @RestController
 class ApplicationController {
 	@GetMapping(value = ["/"])
-	fun hello() = Response("Hello World")
+	fun hello() = Response(System.nanoTime(), "Hello World")
+
+	@GetMapping(value = ["/heapmemtest"])
+	fun heapMemTest(): List<Response> {
+		val list = mutableListOf<Response>()
+		for (i in 1..1000000) {
+			list.add(Response(System.nanoTime(), "test"))
+		}
+		return list
+	}
 }
 
 @SpringBootApplication
